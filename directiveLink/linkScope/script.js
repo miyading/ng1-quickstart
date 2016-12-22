7cffbf33f449575f850450018d2eb6bd30aec1c8
Mengli Ding <mlding@thoughtworks.com>
2016-12-23 07:47:16 +0800

    [Mengli]-angular 1 directive link scope.

--- directiveLink/linkScope/script.js
+++ directiveLink/linkScope/script.js
@@ -2,16 +2,40 @@ var angular = require('angular');
 
 angular.module('directiveLinkScope', [])
   .controller('ExamCtrl', function ($scope) {
-    $scope.money = '123';
+    $scope.reportDetailInfo = {
+      'name': 'productIT',
+      'creator': 'dingding',
+      'createTime': new Date()
+    };
+    $scope.page = 1;
   })
-  .directive('moneyWrap', function () {
+  .directive('reportDetail', function () {
     return {
-      restrict: 'A',
-      transclude: true,
-      scope: {},
-      link: function(scope) {
-        scope.money = '789';
+      restrict: 'E',
+      scope: {
+        reportDetailInfo: '=',
+        page: '='
       },
-      template: '<h1>money: &nbsp;{{money}}</h1>'
+      link: function(scope) {
+        console.log('scope.reportDetailInfo ', scope.reportDetailInfo);
+        console.log('scope.page ', scope.page);
+        scope.page += 1;
+        console.log('scope.page after', scope.page);
+        
+        scope.label = {
+          'name': scope.reportDetailInfo.name + 'dingding',
+          'creator': scope.reportDetailInfo.name + 'mengli'
+        };
+      },
+      templateUrl: './reportDetail.html'
+    }
+  })
+  .directive('configLabel', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        label: '='
+      },
+      templateUrl: './configLabel.html'
     }
   });
